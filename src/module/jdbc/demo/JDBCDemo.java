2f488eed7499b8fb61802949846577534c5c25cf
谢良 <xieliang14798@hundsun.com>
2018-03-27 21:26:20 +0800

    jdbc

---
+++ src/module/jdbc/demo/JDBCDemo.java
@@ -0,0 +1,32 @@
+package module.jdbc.demo;
+
+import module.jdbc.utils.JDBCUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCDemo {
+    public static void main(String[] args) throws SQLException {
+        Connection con = JDBCUtils.getCon();
+        PreparedStatement statement = con.prepareStatement
+                ("SELECT * from users where password = ? ");
+        statement.setObject(1, "23");
+        ResultSet res = statement.executeQuery();
+        List<User> data = new ArrayList<>();
+        while (res.next()) {
+            data.add(new User(
+                    res.getInt("id"),
+                    res.getString("username"),
+                    res.getString("password")
+            ));
+        }
+        JDBCUtils.close(con, statement, res);
+        for (User user : data) {
+            System.out.println(user.toString());
+        }
+    }
+}
---
+++ src/module/jdbc/utils/JDBCUtils.java
@@ -0,0 +1,74 @@
+package module.jdbc.utils;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+public class JDBCUtils {
+    private JDBCUtils() {
+    }
+
+    private static Connection con;
+
+    static {
+        try {
+            /*获取数据库配置*/
+            InputStream in = JDBCUtils.class.getResourceAsStream("database.properties");
+            Properties pro = new Properties();
+            pro.load(in);
+            String driverClass = pro.getProperty("driverClass");
+            String url = pro.getProperty("url");
+            String username = pro.getProperty("username");
+            String password = pro.getProperty("password");
+            /*连接数据库*/
+            Class.forName(driverClass);
+            con = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            throw new RuntimeException(e.toString() + "数据库连接失败");
+        }
+    }
+
+    public static Connection getCon() {
+        return con;
+    }
+
+    public static void close(Connection con, Statement stat) {
+        if(stat!=null){
+            try {
+                stat.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if(con!=null){
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void close(Connection con, Statement stat, ResultSet rs){
+        if(rs!=null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if(stat!=null){
+            try {
+                stat.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if(con!=null){
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
